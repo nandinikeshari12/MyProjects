6189820745d5b294b764f47483ac85de9d8e4877
Mac5 <rajuchakraborty@Mac5s-iMac.local>
2017-10-07 14:31:50 +0530

    HomeVC UI has been done.

--- Daily Deals/HomeVC.h
+++ Daily Deals/HomeVC.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  HomeVC.h
 //  Daily Deals
 //
-//  Created by Mac5 on 06/10/17.
+//  Created by Mac5 on 07/10/17.
 //  Copyright © 2017 Mac5. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HomeVC : UIViewController
 
 @end
-
---
+++ Daily Deals/HomeVC.m
@@ -0,0 +1,33 @@
+//
+//  HomeVC.m
+//  Daily Deals
+//
+//  Created by Mac5 on 07/10/17.
+//  Copyright © 2017 Mac5. All rights reserved.
+//
+
+#import "HomeVC.h"
+
+@interface HomeVC ()
+
+@end
+
+@implementation HomeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)prefersStatusBarHidden
+{
+    return  true;
+}
+
+@end
