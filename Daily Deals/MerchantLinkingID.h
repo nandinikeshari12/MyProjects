af8486ef3655a971882b69f83729b219c1e0002c
Mac5 <rajuchakraborty@Mac5s-iMac.local>
2017-10-07 18:37:38 +0530

    UI has been design updated.

---
+++ Daily Deals/MerchantLinkingID.h
@@ -0,0 +1,13 @@
+//
+//  MerchantLinkingID.h
+//  Daily Deals
+//
+//  Created by Mac5 on 07/10/17.
+//  Copyright © 2017 Mac5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MerchantLinkingID : UIViewController
+
+@end
---
+++ Daily Deals/MerchantLinkingID.m
@@ -0,0 +1,37 @@
+//
+//  MerchantLinkingID.m
+//  Daily Deals
+//
+//  Created by Mac5 on 07/10/17.
+//  Copyright © 2017 Mac5. All rights reserved.
+//
+
+#import "MerchantLinkingID.h"
+
+@interface MerchantLinkingID ()
+
+@end
+
+@implementation MerchantLinkingID
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
